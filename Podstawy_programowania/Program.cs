282eeb88924c2fc3f09e29e4b1af75413c2cfbb4
Kacper Szczepanek <Kacper@MacBook-Pro-Kacper.local>
2019-10-05 22:54:33 +0200

    First commit

---
+++ Podstawy_programowania/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Podstawy_programowania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
