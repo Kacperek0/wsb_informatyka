0dd636c2662ce890d2aff0dac127b9daf8b5536d
Kacperek0 <47487512+Kacperek0@users.noreply.github.com>
2019-10-06 23:52:04 +0200

    Adding next training tasks

---
+++ For_loop_1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Podstawy_programowania
+{
+    public class For_loop_1
+    {
+        public For_loop_1()
+        {
+
+            int i = 1;
+
+            for (i = 0; i <= 10; i++)
+
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+    }
+}
