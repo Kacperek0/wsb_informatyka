c23a12b4d2d7ad57dc4e4d8e73d4d1fd9907e261
Kacper Szczepanek <kacper.szczepanek@gmail.com>
2019-10-18 22:43:03 +0200

    Statring book course

---
+++ Podstawy_programowania/kursLis/Rozdział1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Podstawy_programowania.kursLis
+{
+    /// <summary>Application's subclass</summary>
+    public class Rozdział1
+    {
+        /// <remarks>Method</remarks>
+        public Rozdział1()
+        {
+            Console.WriteLine("To jest napis");
+            ///<value>They've asked to write like this in a book</value>
+        }
+    }
+}
