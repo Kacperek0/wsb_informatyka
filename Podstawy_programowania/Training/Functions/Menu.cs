61cc26485376b012ab5d643d87e0fe89b1e3b0a3
Kacper Szczepanek <kacper.szczepanek@gmail.com>
2019-12-03 23:27:02 +0100

    Functions

---
+++ Podstawy_programowania/Training/Functions/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Podstawy_programowania.Training.Functions
+{
+    public class Menu
+    {
+        public Menu()
+        {
+        }
+
+        public int StartingMenu(int a)
+        {
+            while (true)
+            {
+                if (a == 1)
+                {
+                    return 1;
+                }
+                else if (a == 2)
+                {
+                    return 2;
+                }
+            }
+
+        }
+    }
+}
